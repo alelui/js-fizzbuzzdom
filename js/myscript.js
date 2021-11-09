a3278f85349e91771d7ffa6b03459d36ddd42271
Alessandro <luisialessandro95@gmail.com>
2021-11-09 19:20:23 +0100

    friz solo nei primi

--- js/myscript.js
+++ js/myscript.js
@@ -4,32 +4,34 @@ const ul = document.querySelector('.list');
 
 let i;
 let x;
-let hope; 
+
+let li;
 
 
-
-// for ( i = 1; i <= 15; i++ ){
-//     ul.innerHTML += `<li class="box box-${i}">${i}</li>`;
-//     console.log(i);
-// }
-
-for ( i= 1; i <= 100; i++){
+for ( i= 1; i <= 15; i++){
 
     if ( i % 3 == 0 &&  i % 5 == 0){
         x = 'tre-cin';
         ul.innerHTML += `<li class="box box-${x}">${i}</li>`;
+        li = document.querySelector(`.box-${x}`).innerHTML = 'fzzBzz';
+
 
     }else if ( i % 3 == 0 ){
         x = 'tre';
         ul.innerHTML += `<li class="box box-${x}">${i}</li>`;
+        li = document.querySelector(`.box-${x}`).innerHTML = 'fzz';
+   
+
         
     }else if ( i % 5 == 0 ){
         x = 'cin';
         ul.innerHTML += `<li class="box box-${x}">${i}</li>`;
-        
+        li = document.querySelector(`.box-${x}`).innerHTML = 'Bzz';
+
     }else{
-        ul.innerHTML += `<li class="box">${i}</li>`;
+        ul.innerHTML += `<li class="box">${i}</li>`;  
     }
+    console.log(li);
 }
 
 
