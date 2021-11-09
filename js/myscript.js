b087eacb137d0d53b318bee08a84b518a4635d4e
Alessandro <luisialessandro95@gmail.com>
2021-11-10 00:53:18 +0100

    buzz effect

--- js/myscript.js
+++ js/myscript.js
@@ -6,13 +6,13 @@ let i;
 for ( i = 1; i <= 100; i++){
 
     if ( i % 3 == 0 &&  i % 5 == 0){
-        ul.innerHTML += `<li class="box box-tre-cin"><span>fzzBzz</span></li>`;
+        ul.innerHTML += `<li class="box box-tre-cin"><span>fizzBuzz</span></li>`;
 
     }else if ( i % 3 == 0 ){
-        ul.innerHTML += `<li class="box box-tre"><span>fzz</span></li>`;
+        ul.innerHTML += `<li class="box box-tre"><span>fizz</span></li>`;
    
     }else if ( i % 5 == 0 ){
-        ul.innerHTML += `<li class="box box-cin"><span>Bzz</span></li>`;
+        ul.innerHTML += `<li class="box box-cin"><span>Buzz</span></li>`;
 
     }else{
         ul.innerHTML += `<li class="box">${i}</li>`;  
